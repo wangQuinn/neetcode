6547df5f8d10d254cf52cf90fff46c17603b5bbf
Quinn Wang <99744390+wangQuinn@users.noreply.github.com>
2026-08-25 22:18:50 -0400

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.cpp
@@ -0,0 +1,18 @@
+// let n be the length of nums
+//Time Complexity : O(n) 
+//Space Complexity : O(1) (uses only two variables)
+//this reminds me alot of Maximum Product Subarray 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0]; //guaranteed to have at least one element . 
+        int currentSum = nums[0]; 
+        for(int i = 1; i < nums.size(); i++){
+            //for each, choose to include or to not include;
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(currentSum, maxSum);
+        }
+        return maxSum;
+
+    }
+};
